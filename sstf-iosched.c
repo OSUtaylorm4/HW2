b2b3c56106d22c0326b8d16ae9dc1ec9f49d4241
OSUtaylorm4 <taylorm4@oregonstate.edu>
2016-04-27 20:51:52 -0700

    Finished scheduler
    Wow I need to commit way more often

--- sstf-iosched.c
+++ sstf-iosched.c
@@ -1,124 +1,207 @@
 /*
- * elevator noop
+ * Author:	Matthew Taylor
+ * Computer Science 444 Spring 2016
+ * Implement SSTF Compiler
  */
+
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/init.h>
 
-struct noop_data {
-        struct list_head queue;
+struct look_data {
+    struct list_head queue;
+    sector_t hp;
+    int dir;
 };
 
-static void noop_merged_requests(struct request_queue *q, struct request *rq,
-                                 struct request *next)
+static void look_merged_requests
+(struct request_queue *q, struct request *rq, struct request *next)
 {
-        list_del_init(&next->queuelist);
+    list_del_init(&next->queuelist);
 }
 
-static int noop_dispatch(struct request_queue *q, int force)
+static int look_dispatch(struct request_queue *q, int force)
 {
-        struct noop_data *nd = q->elevator->elevator_data;
+    struct look_data *nd = q->elevator->elevator_data;
+    
+    printk("Here is the start of dispatch\n");
 
-        if (!list_empty(&nd->queue)) {
-                struct request *rq;
-                rq = list_entry(nd->queue.next, struct request, queuelist);
-                list_del_init(&rq->queuelist);
-                elv_dispatch_sort(q, rq);
-                return 1;
-        }
-        return 0;
+    if(!list_empty(&nd->queue))
+    {
+        struct request *nxt_rq, *prv_rq, *rq;
+	
+    	nxt_rq = list_entry(nd->queue.next, struct request, queuelist);
+    	prv_rq = list_entry(nd->queue.prev, struct request, queuelist);
+
+    	if(prv_rq == nxt_rq)
+            {
+                printk("There is one request\n");
+                rq = nxt_rq;   
+            }
+            else
+            {
+                printk("There are mutliple requests\n");
+
+    	    if(nd->dir == 1)
+                {
+                    printk("Queue direction is going forward\n");
+    		
+    		if(nxt_rq->__sector > nd->hp)
+    		    rq = nxt_rq;
+
+    		else
+    		{
+    		    nd->dir = 0;
+                        rq = prv_rq;
+                    }
+                }
+
+    	    else
+    	    {
+    		printk("Queue direction is going backward\n");
+
+    		if(prv_rq->__sector < nd->hp)
+                        rq = prv_rq;
+    		else
+    		{
+    		    nd->dir = 1;
+    		    rq = nxt_rq;
+    		}
+	    }
+	}
+
+	printk("<NOW EXECUTING>\n");   
+        //Remove it from the queue we just got it from
+        list_del_init(&rq->queuelist);
+        //get the new position for the read head
+        nd->hp = blk_rq_pos(rq) + blk_rq_sectors(rq);
+        //give request to the elevator 
+        elv_dispatch_add_tail(q, rq);
+        printk("Things have finished.\n");
+        printk("LOOK accessing %llu\n",(unsigned long long) rq->__sector);
+        return 1;
+    }   
+    return 0;
 }
 
-static void noop_add_request(struct request_queue *q, struct request *rq)
-{
-        struct noop_data *nd = q->elevator->elevator_data;
 
-        list_add_tail(&rq->queuelist, &nd->queue);
+static void look_add_request(struct request_queue *q, struct request *rq)
+{
+    struct look_data *nd = q->elevator->elevator_data;
+    struct request *nxt_rq, *prv_rq;
+ 
+    printk("Starting add\n");
+
+    if(list_empty(&nd->queue))
+    {
+        printk("The list is empty\n");
+        list_add(&rq->queuelist, &nd->queue);
+    }
+    else
+    {
+        printk("Locating position for req\n");
+
+	nxt_rq = list_entry(nd->queue.next, struct request, queuelist);
+    prv_rq = list_entry(nd->queue.prev, struct request, queuelist);
+
+	 while(blk_rq_pos(rq) > blk_rq_pos(nxt_rq))
+    {
+        nxt_rq = list_entry(nxt_rq->queuelist.next, struct request, queuelist);
+        prv_rq = list_entry(prv_rq->queuelist.prev, struct request, queuelist);
+    }
+
+	list_add(&rq->queuelist, &prv_rq->queuelist);
+        printk("Found the position\n");
+    }
+    printk("SSTF adding %llu\n", (unsigned long long) rq->__sector);
 }
 
-static struct request *
-noop_former_request(struct request_queue *q, struct request *rq)
-{
-        struct noop_data *nd = q->elevator->elevator_data;
 
-        if (rq->queuelist.prev == &nd->queue)
-                return NULL;
-        return list_entry(rq->queuelist.prev, struct request, queuelist);
+static struct request * 
+look_former_request(struct request_queue *q, struct request *rq)
+{
+    struct look_data *nd = q->elevator->elevator_data;
+
+    if(rq->queuelist.prev == &nd->queue)
+        return NULL;
+    return list_entry(rq->queuelist.prev, struct request, queuelist);
 }
 
-static struct request *
-noop_latter_request(struct request_queue *q, struct request *rq)
+static struct request * 
+look_latter_request(struct request_queue *q, struct request *rq)
 {
-        struct noop_data *nd = q->elevator->elevator_data;
+    struct look_data *nd = q->elevator->elevator_data;
 
-        if (rq->queuelist.next == &nd->queue)
-                return NULL;
-        return list_entry(rq->queuelist.next, struct request, queuelist);
+    if(rq->queuelist.next == &nd->queue)
+        return NULL;
+    return list_entry(rq->queuelist.next, struct request, queuelist);
 }
 
-static int noop_init_queue(struct request_queue *q, struct elevator_type *e)
+static int look_init_queue(struct request_queue *q, struct elevator_type *e)
 {
-        struct noop_data *nd;
-        struct elevator_queue *eq;
+    struct look_data *nd;
+    struct elevator_queue *eq;
 
-        eq = elevator_alloc(q, e);
-        if (!eq)
-                return -ENOMEM;
+    eq = elevator_alloc(q, e);
+    if(!eq)
+        return -ENOMEM;
 
-        nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
-        if (!nd) {
-                kobject_put(&eq->kobj);
-                return -ENOMEM;
-        }
-        eq->elevator_data = nd;
+    nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
+    if(!nd)
+    {
+       kobject_put(&eq->kobj);
+        return -ENOMEM;
+    }
+  
+    nd->hp = 0;
+    eq->elevator_data = nd;
 
-        INIT_LIST_HEAD(&nd->queue);
-
-        spin_lock_irq(q->queue_lock);
-        q->elevator = eq;
-        spin_unlock_irq(q->queue_lock);
-        return 0;
+    INIT_LIST_HEAD(&nd->queue);
+    spin_lock_irq(q->queue_lock);
+    q->elevator = eq;
+    spin_unlock_irq(q->queue_lock);
+    return 0;
 }
 
-static void noop_exit_queue(struct elevator_queue *e)
+static void look_exit_queue(struct elevator_queue *e)
 {
-        struct noop_data *nd = e->elevator_data;
+    struct look_data *nd = e->elevator_data;
 
-        BUG_ON(!list_empty(&nd->queue));
-        kfree(nd);
+    BUG_ON(!list_empty(&nd->queue));
+    kfree(nd);
 }
 
-static struct elevator_type elevator_noop = {
-        .ops = {
-                .elevator_merge_req_fn          = noop_merged_requests,
-                .elevator_dispatch_fn           = noop_dispatch,
-                .elevator_add_req_fn            = noop_add_request,
-                .elevator_former_req_fn         = noop_former_request,
-                .elevator_latter_req_fn         = noop_latter_request,
-                .elevator_init_fn               = noop_init_queue,
-                .elevator_exit_fn               = noop_exit_queue,
-        },
-        .elevator_name = "noop",
-        .elevator_owner = THIS_MODULE,
+static struct elevator_type elevator_look = {
+    .ops = {
+        .elevator_merge_req_fn = look_merged_requests,
+        .elevator_dispatch_fn = look_dispatch,
+        .elevator_add_req_fn = look_add_request,
+        .elevator_former_req_fn = look_former_request,
+        .elevator_latter_req_fn = look_latter_request,
+        .elevator_init_fn = look_init_queue,
+        .elevator_exit_fn = look_exit_queue,
+    },
+    .elevator_name = "sstf",
+    .elevator_owner = THIS_MODULE,
+
 };
 
-static int __init noop_init(void)
+static int __init look_init(void)
 {
-        return elv_register(&elevator_noop);
+	return elv_register(&elevator_look);
 }
 
-static void __exit noop_exit(void)
+static void __exit look_exit(void)
 {
-        elv_unregister(&elevator_noop);
+	elv_unregister(&elevator_look);
 }
 
-module_init(noop_init);
-module_exit(noop_exit);
+module_init(look_init);
+module_exit(look_exit);
 
 
-MODULE_AUTHOR("Jens Axboe");
+MODULE_AUTHOR("Matthew Taylor");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("No-op IO scheduler");
+MODULE_DESCRIPTION("SSTF I/O scheduler -- CS 444 Spring 2016");
